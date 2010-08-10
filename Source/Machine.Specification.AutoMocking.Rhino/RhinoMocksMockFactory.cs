ff06381545f65a34c319c9cbfd6f8ef330815ca4
Roelof Blom <roelof.blom@gmail.com>
2010-08-10 13:35:01 +0200

    Fixed namespace and added extension methods for expectations.

--- Source/Machine.Specification.AutoMocking.Rhino/RhinoMocksMockFactory.cs
+++ Source/Machine.Specification.AutoMocking.Rhino/RhinoMocksMockFactory.cs
@@ -1,23 +1,19 @@
+using System;
+using Machine.Specifications.AutoMocking.Core;
+using Rhino.Mocks;
+
 namespace Machine.Specifications.AutoMocking.Rhino
 {
-    using System;
-    using Core;
-    using global::Rhino.Mocks;
+	public class RhinoMocksMockFactory : IMockFactory
+	{
+		public Dependency create_stub<Dependency>() where Dependency : class
+		{
+			return MockRepository.GenerateStub<Dependency>();
+		}
 
-    public class RhinoMocksMockFactory : IMockFactory
-    {
-        #region MockFactory Members
-
-        public Dependency create_stub<Dependency>() where Dependency : class
-        {
-            return MockRepository.GenerateStub<Dependency>();
-        }
-
-        public object create_stub(Type type)
-        {
-            return MockRepository.GenerateStub(type);
-        }
-
-        #endregion
-    }
+		public object create_stub(Type type)
+		{
+			return MockRepository.GenerateStub(type);
+		}
+	}
 }
--- Source/Machine.Specification.AutoMocking.Rhino/Specification.cs
+++ Source/Machine.Specification.AutoMocking.Rhino/Specification.cs
@@ -1,11 +1,11 @@
 namespace Machine.Specifications.AutoMocking.Rhino
 {
-    public abstract class Specification<TSubject> : Specification<TSubject, TSubject>
-    {
-    }
+	public abstract class Specification<TSubject> : Specification<TSubject, TSubject>
+	{
+	}
 
-    public abstract class Specification<TContract, TSubject> :
-        Specification<TContract, TSubject, RhinoMocksMockFactory> where TSubject : TContract
-    {
-    }
+	public abstract class Specification<TContract, TSubject> :
+		Specification<TContract, TSubject, RhinoMocksMockFactory> where TSubject : TContract
+	{
+	}
 }
