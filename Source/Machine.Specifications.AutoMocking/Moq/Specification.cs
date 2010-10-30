b2e8e6c883d4b91e27f64185cd29a15612a2fa3f
unknown <dan@.(none)>
2010-10-30 19:19:50 +0100

    Convenience method to return Mocked instances directly as a member function of Specification.

--- Source/Machine.Specifications.AutoMocking/Moq/Specification.cs
+++ Source/Machine.Specifications.AutoMocking/Moq/Specification.cs
@@ -1,3 +1,4 @@
+using Moq;
 namespace Machine.Specifications.AutoMocking.Moq
 {
 	public abstract class Specification<TSubject> : Specification<TSubject, TSubject>
@@ -7,5 +8,9 @@ namespace Machine.Specifications.AutoMocking.Moq
 	public abstract class Specification<TContract, TSubject> :
 		Specification<TContract, TSubject, MoqMocksMockFactory> where TSubject : TContract
 	{
+        public virtual Mock<T> MockDependencyOf<T>() where T: class
+        {
+            return Mock.Get<T>(DependencyOf<T>());
+        }
 	}
 }
