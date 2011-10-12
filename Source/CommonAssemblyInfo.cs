dbcd30c403401296cb979e706835e217efe7d196
Joe Feser <joseph.feser@gmail.com>
2011-10-12 00:04:00 -0400

    version is now in sync with the mspec version

--- Source/CommonAssemblyInfo.cs
+++ Source/CommonAssemblyInfo.cs
@@ -15,8 +15,8 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("0.1.5")]
-[assembly: AssemblyFileVersion("0.1.5.0")]
+[assembly: AssemblyVersion("1.4.24")]
+[assembly: AssemblyFileVersion("1.4.24.0")]
 [assembly: AssemblyCopyright("Copyright (c) James Broome 2011")]
 [assembly: AssemblyConfiguration("Debug")]
 [assembly: AssemblyCompany("James Broome")]
